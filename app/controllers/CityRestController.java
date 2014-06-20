89c797b3221ad1969f16f2b5bab2f149e26c737c
Jurijs Kolomijecs <jurikolo@yandex.com>
2014-06-20 15:54:02 +0300

    Added CityRest controller

---
+++ app/controllers/CityRestController.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import static play.libs.Json.toJson;
+
+import javax.inject.Inject;
+
+import com.google.common.collect.ImmutableMap;
+
+import models.City;
+import models.dao.CityDAO;
+import play.mvc.Result;
+import play.utils.crud.APIController;
+
+public class CityRestController extends APIController<Long, Sample> {
+
+	@Inject
+	public CityRestController(CityDAO dao) {
+		super(dao, Long.class, City.class);
+	}
+
+	@Override
+	public Result create() {
+		Result check = checkRequired("name");
+		if (check != null) {
+			if (log.isDebugEnabled())
+				log.debug("check : " + check);
+			return check;
+		}
+
+		String name = jsonText("name");
+
+		City m = new City();
+		m.setName(name);
+		
+		Long key = dao.create(m);
+		if (log.isDebugEnabled())
+			log.debug("key : " + key);
+
+		return created(toJson(ImmutableMap.of("status", "OK", "key", key)));
+	}
+
+}
