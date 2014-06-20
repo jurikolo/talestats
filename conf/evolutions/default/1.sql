926806ef866082f60be63dcc2b289bb9cc937896
Jurijs Kolomijecs <jurikolo@yandex.com>
2014-06-20 14:01:44 +0300

    Full crud sample from https://github.com/hakandilek/play2-crud/tree/master/samples/play2-crud-sample

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,26 +3,13 @@
 
 # --- !Ups
 
-create table complex (
-  key                       bigint not null,
-  string_field              varchar(255),
-  long_string_field         varchar(256),
-  integer_field             integer,
-  double_field              double,
-  boolean_field             boolean,
-  date_field                timestamp,
-  constraint pk_complex primary key (key))
-;
-
-create table simple (
+create table sample (
   key                       bigint not null,
   name                      varchar(255),
-  constraint pk_simple primary key (key))
+  constraint pk_sample primary key (key))
 ;
 
-create sequence complex_seq;
-
-create sequence simple_seq;
+create sequence sample_seq;
 
 
 
@@ -31,13 +18,9 @@ create sequence simple_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table if exists complex;
-
-drop table if exists simple;
+drop table if exists sample;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
-drop sequence if exists complex_seq;
-
-drop sequence if exists simple_seq;
+drop sequence if exists sample_seq;
 
