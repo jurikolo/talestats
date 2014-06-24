16e904de88124717c000a794ca80e62698f53daa
Jurijs Kolomijecs <jurikolo@yandex.com>
2014-06-24 12:48:52 +0300

    Added simple scheduler to log

---
+++ app/Global.java
@@ -0,0 +1,31 @@
+import play.Application;
+import play.GlobalSettings;
+import play.libs.Akka;
+import scala.concurrent.duration.FiniteDuration;
+
+import java.lang.reflect.Method;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Global extends GlobalSettings {
+	@Override
+	public void onStart(Application app) {
+		// Magic goes here
+		FiniteDuration delay = FiniteDuration.create(0, TimeUnit.SECONDS);
+		FiniteDuration frequency = FiniteDuration.create(5, TimeUnit.SECONDS);
+		Runnable showTime = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Time is now: " + new Date());
+			}
+		};
+
+		Akka.system()
+				.scheduler()
+				.schedule(delay, frequency, showTime,
+						Akka.system().dispatcher());
+	}
+
+}
