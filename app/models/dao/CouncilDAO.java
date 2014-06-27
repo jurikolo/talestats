d03c43fcd198e8111014f3ea7f3a1cc8d742824a
Jurijs Kolomijecs <jurikolo@yandex.com>
2014-06-27 17:12:42 +0300

    Nothing to add

--- app/models/dao/CouncilDAO.java
+++ app/models/dao/CouncilDAO.java
@@ -1,12 +1,27 @@
 package models.dao;
 
+import play.Logger;
+import play.Logger.ALogger;
 import play.utils.dao.BasicDAO;
 import models.Council;
 
 public class CouncilDAO extends BasicDAO<Integer, Council> {
+	
+	private static ALogger log = Logger.of(CouncilDAO.class);
 
 	public CouncilDAO() {
 		super(Integer.class, Council.class);
 	}
-	
+
+	public void saveOrUpdate(Council council) {
+		if (null == council) 
+			throw new RuntimeException("Council is NULL!!!");
+		
+		if (null == get(council.getKey())) {
+			log.debug("Council not found");
+			create(council);
+		} else {
+			update(council);
+		}
+	}
 }
